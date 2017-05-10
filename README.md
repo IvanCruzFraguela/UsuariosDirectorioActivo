17cb3ef0e324d734cccdb309e3d5381431cf8422
Ivan Cruz Fraguela <ivancruzfraguela@gmail.com>
2017-05-10 08:40:36 +0200

    Programa hecho

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # UsuariosDirectorioActivo
-La idea original es crear un fichero de texto con los nombres y nombres de usuario almacenados en un Directorio Activo
+La idea original es crear un fichero de texto con los nombres y nombres de usuario almacenados en un Directorio Activo y, de momento es lo que tenemos.
+
+El programa recibe dos parámetros:
+El primero el la entrada de directorio en donde se encuentren los usuarios a meter en el fichero. Es obligatorio ponerlo.
+El segundo es el fichero donde se guardarán esos datos.
+
+
--- udaExe/Program.cs
+++ udaExe/Program.cs
@@ -4,9 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+
 namespace udaExe {
 	class Program {
 		static void Main(string[] args) {
+			string ldapEntry;
+			string fileName;
+			int cantUsuarios = 0;
+			if (!GetParameters(args, out ldapEntry, out fileName)) {
+				Console.Out.WriteLine("uso: udaExe ldap fichero");
+				Console.Out.WriteLine("Ejemplo: udaExe \"LDAP://OU=Users,dc=microsoft,dc=local\" \"c:\\usuarios.txt\"");
+
+			} else {
+				Console.Out.WriteLine("Buscando usuarios:");
+
+				using (StreamWriter sw = File.CreateText(fileName)) {
+					try {
+						DirectoryEntry directoryObject = new DirectoryEntry(ldapEntry);
+						foreach (DirectoryEntry child in directoryObject.Children) {
+							string displayName = GetProperty(child, "displayName");
+							string distinguishedName = GetProperty(child, "distinguishedName");
+							string samAccountName = GetProperty(child, "samAccountName");
+							child.Close();
+							child.Dispose();
+							sw.WriteLine($"{displayName}#{samAccountName}#{distinguishedName}");
+							cantUsuarios++;
+						}
+						directoryObject.Close();
+						directoryObject.Dispose();
+					} catch (DirectoryServicesCOMException ex) {
+						Console.Error.WriteLine("Error: " + ex.Message.ToString());
+					}
+				}
+				Console.Out.WriteLine(cantUsuarios.ToString() + " usuarios encontrados, fichero generado: " + fileName);
+			}
+		}
+
+		public static string GetProperty(DirectoryEntry de, string PropertyName) {
+			if (de.Properties.Contains(PropertyName)) {
+				return de.Properties[PropertyName][0].ToString();
+			} else {
+				return string.Empty;
+			}
+		}
+
+		public static bool GetParameters(string[] args, out string ldap, out string fileName) {
+			ldap = String.Empty;
+			fileName = @"c:\users.txt";
+			if (args.Length < 1) {
+				return false;
+			}
+			ldap = args[0];
+			if (args.Length >= 2) {
+				fileName = args[1];
+			}
+			return true;
 		}
 	}
 }
